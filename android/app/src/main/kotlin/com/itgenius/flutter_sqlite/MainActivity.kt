4a6e03b7d3f7c4a3652aee3864cf99a438bfb53c
Samit Koyom <samitkoyom@gmail.com>
2022-10-27 01:31:31 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/itgenius/flutter_sqlite/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.itgenius.flutter_sqlite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: prefer_const_declarations, prefer_const_constructors, use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_sqlite/page/notes_page.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = 'Notes SQLite';
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: title,
+    themeMode: ThemeMode.dark,
+    theme: ThemeData(
+      primaryColor: Colors.black,
+      scaffoldBackgroundColor: Colors.blueGrey.shade900,
+      appBarTheme: AppBarTheme(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+    ),
+    home: NotesPage(),
+  );
+}
